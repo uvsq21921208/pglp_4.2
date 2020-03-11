6e20aff4ee5588b91ae04b8787a9b92196fdbac2
uvsq21921208 <mouttiedje@outlook.com>
2020-03-11 12:13:42 +0100

    Initial commit

---
+++ pglp_4.2/src/main/java/uvsq2192128/pglp_4_2/App.java
@@ -0,0 +1,13 @@
+package uvsq2192128.pglp_4_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
